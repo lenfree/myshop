08caef60144bc63a685e43e4edbd30ac3968f633
Lenfree Yeung <lenfree.yeung@gmail.com>
2019-05-09 22:57:45 +1000

    check error enable for dev env

--- config/dev.exs
+++ config/dev.exs
@@ -11,6 +11,7 @@ config :myshop, MyshopWeb.Endpoint,
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
+  catch_errors: true,
   watchers: [
     node: [
       "node_modules/webpack/bin/webpack.js",
