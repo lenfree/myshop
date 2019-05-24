95b199d3831fd87069066a4dab910eca3be08dc2
Lenfree Yeung <lenfree.yeung@gmail.com>
2019-05-24 15:44:18 +1000

    add cancel order item

--- lib/myshop_web/views/manageorder_view.ex
+++ lib/myshop_web/views/manageorder_view.ex
@@ -1,3 +1,10 @@
 defmodule MyshopWeb.ManageorderView do
   use MyshopWeb, :view
+
+  def compute_total(list), do: sum(list, 0)
+  defp sum([], acc), do: acc
+
+  defp sum([head | tail], acc) do
+    sum(tail, head.product.sell_price + acc)
+  end
 end
