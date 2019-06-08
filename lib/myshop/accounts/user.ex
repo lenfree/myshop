1385674773b8c3559fbc7c89fca2350b5d61993c
Lenfree Yeung <lenfree.yeung@gmail.com>
2019-06-08 16:33:35 +1000

    Add role for customer:

--- lib/myshop/accounts/user.ex
+++ lib/myshop/accounts/user.ex
@@ -6,6 +6,7 @@ defmodule Myshop.Accounts.User do
   schema "users" do
     field :first_name, :string
     field :last_name, :string
+    field :role, :string, default: "customer"
     has_one(:credential, Credential)
 
     timestamps()
@@ -14,7 +15,7 @@ defmodule Myshop.Accounts.User do
   @doc false
   def changeset(user, attrs) do
     user
-    |> cast(attrs, [:first_name, :last_name])
+    |> cast(attrs, [:first_name, :last_name, :role])
     |> validate_required([:first_name, :last_name])
     |> validate_length(:first_name, min: 3, max: 20)
   end
